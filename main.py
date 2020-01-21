387b7bb83776a625c73d86985c61ae8067ddcc3f
CanerF <52736554+CanerF@users.noreply.github.com>
2020-01-21 19:40:47 +0300

    Update main.py
    v3

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ class ISPARK():
         __dataFrame = self.__dataFrame.drop(droplist, 0)
         return __dataFrame
     def set_column_lowercase(self, column_name):
-        __dataFrame = self.__dataFrame[column_name].str.lower()
+        __dataFrame = self.__dataFrame[column_name]
         return __dataFrame
 
 # Dataframe Features
-print(ISPARK(df_Ispark).getColumns())
+#print(ISPARK(df_Ispark).getColumns())
 
 # Dataframe Info
-print(ISPARK(df_Ispark).getInfo())
+#print(ISPARK(df_Ispark).getInfo())
 
 # Corr Map
 #f,ax = plt.subplots(figsize=(18, 18))
@@ -48,7 +48,7 @@ print(ISPARK(df_Ispark).getInfo())
 #plt.title("Correlation Chart")
 #plt.show()
 
-# DATA CLEANING
+# -- DATA CLEANING --
 
 # Section - 1 | Splitting -Tarifesi- Feature
 featureTarifesi = ISPARK(df_Ispark).column_split("Tarifesi",";", False, False)
@@ -67,35 +67,29 @@ df_Ispark["Saat48"] = tarife48['Saat48'].str.lower()
 df_Ispark["Ücret48"] = tarife48['Ücret48']
 
 
+# Section - 2 | Handling Data Types
+def delete_spaces(dataframe):
+    counter = 0
+    for value in dataframe:
+        value = value.replace(" ", "")
+        dataframe.iloc[counter] = value
+        counter += 1
+    return dataframe
 
-#def drop_unwanted_rows(dataframe, wanted_feature, wanted_value, wanted_value2):
-#    indexNames = list(dataframe.index.values)
-#    counter = 0
-#    wanted_value += "  "
-#    for i in df_Ispark[wanted_feature]:
-#        if i != wanted_value or i != wanted_value2 and counter == indexNames[counter]:
-#            dataframe = dataframe.drop(counter)
-#            counter += 1
-#        else:
-#            counter += 1
-#    return dataframe
+def delete_unwanted_rows(dataframe,feauture, wanted_value):
+    counter = 0
+    for value in feauture:
+        if value != wanted_value:
+            dataframe = ISPARK(dataframe).drop_row(counter)
+            counter +=1
+        else:
+            counter +=1
+            continue
+    return dataframe
 
-#df_Ispark = drop_unwanted_rows(df_Ispark,"Saat01", "0-1 Saat","0-1 saat")
-#df_Ispark = drop_unwanted_rows(df_Ispark,"Saat12", "1-2 Saat","1-2 saat")
-#df_Ispark = drop_unwanted_rows(df_Ispark,"Saat24", "2-4 Saat","2-4 saat")
-#df_Ispark = drop_unwanted_rows(df_Ispark,"Saat48", "4-8 Saat","4-8 saat")
-
-
-
-
-
-
-
-
-
-
-
-
+df_Ispark['Saat01'] = df_Ispark['Saat01'].astype(str)
+df_Ispark["Saat01"] = delete_spaces(df_Ispark["Saat01"])
+df_Ispark = delete_unwanted_rows(df_Ispark,df_Ispark['Saat01'], "0-1saat")
 
 
 
